39016e47b4857b0854bb441766ce8f2ccdb7c0e7
Chris Johnson <dada.republic@gmail.com>
2014-08-21 22:31:28 -0400

    updated so GA script doesn't fire for localhost

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,7 @@
-var markdown = require('markdown').markdown
-  , fs = require('fs');
+var markdown = require('markdown').markdown, 
+	fs = require('fs');
+
+var envcheck = true;
 
 exports.markdown = function(req, res){
 
@@ -12,12 +14,18 @@ exports.markdown = function(req, res){
     res.status(404); 
     res.render("index", html = { title: '4(dada republic)4', error:'File not found' });
   }*/
+  
+  if(req.host === 'localhost') {
+	envcheck = false; //if localhost don't include GA script
+  }
+  
 
   fs.readFile('public/markdown/'+file, {"encoding":"utf8"}, function(err, data){
     html = {
       title:'dada republic',
       body:markdown.toHTML(data),
-      UA: true,
+      //UA: true,
+      UA: envcheck,
       //UA: process.env.UA || null,
       domain: process.env.DOMAIN || null
     }
